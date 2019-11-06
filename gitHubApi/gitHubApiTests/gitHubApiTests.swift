c89db628945cb13e0dc8e6ae7302cbedae97561c
Juan Quiroga <juan.quiroga@globant.com>
2019-11-06 14:12:11 -0500

    start Unit Test

--- gitHubApi/gitHubApiTests/gitHubApiTests.swift
+++ gitHubApi/gitHubApiTests/gitHubApiTests.swift
@@ -10,18 +10,29 @@ import XCTest
 @testable import gitHubApi
 
 class gitHubApiTests: XCTestCase {
-
+    var searchVc: SearchScreenViewController!
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        searchVc = SearchScreenViewController()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+        
     }
 
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // given
+        let input = "Juanelo"
+        let output = searchVc.getResutls(searchStr: input)
+        // Then
+//            XCTAssertTrue(spy.didSomething, "someMethod() should do  this")
+//            XCTAssertEqual(output, "SomeOtherString", "someMethod() should return the correct string")
+        
     }
 
     func testPerformanceExample() {
